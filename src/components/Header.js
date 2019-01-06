2e24c7faaaa753a332ecb6d76f73f06b09c85281
Daniel <dansto13@yahoo.com>
2019-01-06 19:12:27 +0100

    Style navigation.

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,6 @@ export default class Header extends React.Component {
   }
 
   render() {
-    console.log(this.state);
     return (
       <header>
         <MenuButton onMenuButtonClick={this.onMenuButtonClick} />
--- src/components/NavigationMenu.js
+++ src/components/NavigationMenu.js
@@ -1,31 +1,52 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import portrait from '../assets/images/portrait.png';
 
 export default class NavigationMenu extends Component {
   render() {
-    console.log(this.props);
+    console.log(portrait);
     return (
       <nav className="menu">
-        <div className="menu__brand" />
+        <div className="menu__brand">
+          <div className="portrait" />
+        </div>
 
         <ul className="menu__navigation">
           <li className="menu__navigation__item">
-            <NavLink to="/" activeClassName="is-active" exact={true}>
+            <NavLink
+              to="/"
+              activeClassName="is-active"
+              className="menu__navigation__item__link"
+              exact={true}
+            >
               Home
             </NavLink>
           </li>
           <li className="menu__navigation__item">
-            <NavLink to="/about" activeClassName="is-active">
+            <NavLink
+              to="/about"
+              activeClassName="is-active"
+              className="menu__navigation__item__link"
+            >
               About
             </NavLink>
           </li>
           <li className="menu__navigation__item">
-            <NavLink to="/portfolio" activeClassName="is-active" exact={true}>
+            <NavLink
+              to="/portfolio"
+              activeClassName="is-active"
+              className="menu__navigation__item__link"
+              exact={true}
+            >
               Portfolio
             </NavLink>
           </li>
           <li className="menu__navigation__item">
-            <NavLink to="/contact" activeClassName="is-active">
+            <NavLink
+              to="/contact"
+              activeClassName="is-active"
+              className="menu__navigation__item__link"
+            >
               Contact
             </NavLink>
           </li>
