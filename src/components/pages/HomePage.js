c30382dd0b900cd9c6c4ee82bd5ef1bd647c66a3
Daniel <dansto13@yahoo.com>
2019-01-20 21:10:16 +0100

    Update css.

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -3,11 +3,12 @@ import React from 'react';
 const Homepage = () => {
   return (
     <main>
+      <h1>Hi,</h1>
       <h1>
-        Daniel
-        <span className="text-secondary"> Stoica</span>
+        I'm<span className="text-secondary"> Daniel,</span>
       </h1>
-      <h2>Web Developer, Programmer, Designer & Entrepreneur</h2>
+      <h1>web developer.</h1>
+      <h2> Frontend Developer / QA Engineer / JavaScript Enthusiast</h2>
     </main>
   );
 };
