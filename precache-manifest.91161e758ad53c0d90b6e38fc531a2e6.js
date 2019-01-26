fd0d16ddbb45c262242e8ab00940f8d76bb66b13
Daniel <dansto13@yahoo.com>
2019-01-26 19:26:45 +0100

    Updates

---
+++ precache-manifest.91161e758ad53c0d90b6e38fc531a2e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "989d06acef029a3117e27f1536ffa8b8",
+    "url": "/dev-website/static/media/portrait.989d06ac.png"
+  },
+  {
+    "revision": "e6fa893e41c38b908989b77889c55c83",
+    "url": "/dev-website/static/media/logo.e6fa893e.png"
+  },
+  {
+    "revision": "0bcc732cab194bfa730d02ab86a6b6fb",
+    "url": "/dev-website/static/media/laptop.0bcc732c.jpg"
+  },
+  {
+    "revision": "f0adcfd567585a1f9fd5",
+    "url": "/dev-website/static/js/runtime~main.f0adcfd5.js"
+  },
+  {
+    "revision": "bf7ac087b9d400303bee",
+    "url": "/dev-website/static/js/main.bf7ac087.chunk.js"
+  },
+  {
+    "revision": "b8cf2debfaba7abdc078",
+    "url": "/dev-website/static/js/1.b8cf2deb.chunk.js"
+  },
+  {
+    "revision": "bf7ac087b9d400303bee",
+    "url": "/dev-website/static/css/main.275baeac.chunk.css"
+  },
+  {
+    "revision": "97f9aa9719c00127af08b8e3341f0802",
+    "url": "/dev-website/index.html"
+  }
+];
