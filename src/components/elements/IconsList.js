4ba51ffb75306a2c7acba29af140e038da32bdf0
Daniel <dansto13@yahoo.com>
2019-01-30 00:06:05 +0100

    Update styles.

--- src/components/elements/IconsList.js
+++ src/components/elements/IconsList.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 export class IconsList extends Component {
   render() {
     return (
-      <div className="icons">
+      <div className="icons slide-up">
+        <Link to="/portfolio">
+          <i className="fas fa-briefcase fa-2x" />
+        </Link>
         <Link to="/" target="_blank" rel="noopener">
           <i className="fab fa-linkedin fa-2x" />
         </Link>
         <Link to="/" target="_blank" rel="noopener">
           <i className="fab fa-github fa-2x" />
-        </Link>
-        <Link to="/" target="_blank" rel="noopener">
-          <i className="fab fa-facebook fa-2x" />
         </Link>
       </div>
     );
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,21 +5,21 @@ import IconsList from '../elements/IconsList';
 const Homepage = () => {
   return (
     <main>
-      <h1>
+      <h1 className="slide-down">
         Hi<span className="text-secondary">.</span>
       </h1>
-      <h1>
+      <h1 className="slide-down">
         I<span className="text-secondary">'</span>m
         <span className="text-secondary"> Daniel</span>.
       </h1>
-      <h2>
+      <h2 className="slide-up">
         I'm a Web & QA <span className="text-secondary">Developer</span> based
         in Eindhoven, Netherlands.
       </h2>
-      <Link to="/contact" className="contact-button">
+      <Link to="/contact" className="contact-button slide-up">
         <i className="fas fa-envelope fa-1x" /> Say Hi!
       </Link>
-      <IconsList className="icons" />
+      <IconsList className="icons slide-up" />
     </main>
   );
 };
